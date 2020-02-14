26e7c654d8f93f3f124c3605b15b0f677804a905
SungChulByun <sungchul.byun@navercorp.com>
2020-02-14 13:37:36 +0900

    0214 noon

--- ViewPager_Practice_Byun/app/src/main/java/com/example/viewpager_practice_byun/ChatAdapter.java
+++ ViewPager_Practice_Byun/app/src/main/java/com/example/viewpager_practice_byun/ChatAdapter.java
@@ -2,9 +2,12 @@ package com.example.viewpager_practice_byun;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.viewpager_practice_byun.CustomData.dataBlog;
 
 public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -31,5 +34,25 @@ class NewsHolder extends RecyclerView.ViewHolder{
 
     public NewsHolder(@NonNull View itemView) {
         super(itemView);
+    }
+}
+class BlogHolder extends RecyclerView.ViewHolder{
+    private TextView title;
+    private TextView description;
+    private TextView blogName;
+    private TextView blogLink;
+
+    public BlogHolder(@NonNull View itemView) {
+        super(itemView);
+        title = itemView.findViewById(R.id.blog_title);
+        description = itemView.findViewById(R.id.blog_description);
+        blogName = itemView.findViewById(R.id.blog_name);
+        blogLink = itemView.findViewById(R.id.blog_link);
+    }
+
+    public void setData(dataBlog data){
+        title.setText(data.getTitle());
+        description.setText(data.getDescription());
+        blogName.setText(data.getName());
     }
 }
