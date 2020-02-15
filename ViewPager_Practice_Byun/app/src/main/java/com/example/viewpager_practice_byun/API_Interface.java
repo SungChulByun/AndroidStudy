aca990021dcc54412aade2f8e12cca9588610679
SungChulByun <sungchul.byun@navercorp.com>
2020-02-15 16:49:25 +0900

    02/15 17:00

--- ViewPager_Practice_Byun/app/src/main/java/com/example/viewpager_practice_byun/API_Interface.java
+++ ViewPager_Practice_Byun/app/src/main/java/com/example/viewpager_practice_byun/API_Interface.java
@@ -9,29 +9,29 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface API_Interface {
-    String clientid = "nnfdZXpFFzGmrG5JGWMt";
-    String secret = "8B3emomXbq";
+    String clientid = "xtRJzO1RX4ruym2PKeao";
+    String secret = "Be7KOxAwMi";
     String appName = "ByunSungChul_ViewPager";
 
 
     @Headers({
-            "X-Naver-Client-Id: nnfdZXpFFzGmrG5JGWMt",
-            "X-Naver-Client-Secret: 8B3emomXbq"
+            "X-Naver-Client-Id: xtRJzO1RX4ruym2PKeao",
+            "X-Naver-Client-Secret: Be7KOxAwMi"
     })
     @GET("/v1/search/{type}")
     Call<Image_Result> search_Image(@Path("type") String stype , @Query("query") String text);
 
     @Headers({
-            "X-Naver-Client-Id: nnfdZXpFFzGmrG5JGWMt",
-            "X-Naver-Client-Secret: 8B3emomXbq"
+            "X-Naver-Client-Id: xtRJzO1RX4ruym2PKeao",
+            "X-Naver-Client-Secret: Be7KOxAwMi"
     })
     @GET("/v1/search/{type}")
     Call<Blog_Result> search_Blog(@Path("type") String stype , @Query("query") String text);
 
 
     @Headers({
-            "X-Naver-Client-Id: nnfdZXpFFzGmrG5JGWMt",
-            "X-Naver-Client-Secret: 8B3emomXbq"
+            "X-Naver-Client-Id: xtRJzO1RX4ruym2PKeao",
+            "X-Naver-Client-Secret: Be7KOxAwMi"
     })
     @GET("/v1/search/{type}")
     Call<News_Result> search_News(@Path("type") String stype , @Query("query") String text);
